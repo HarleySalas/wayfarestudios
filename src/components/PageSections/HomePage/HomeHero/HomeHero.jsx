d274a167b6acbaf68654cc5329132b10e1692e71
Harley Salas <harley@harleysalas.com>
2019-12-19 23:27:07 +0300

    created homehero

---
+++ src/components/PageSections/HomePage/HomeHero/HomeHero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./HomeHero.scss";
+
+import HeroImg from "./assets/home-hero.jpg";
+
+import Button from "../../../Button/Button";
+
+const HomeHero = () => {
+  return (
+    <header className="home-hero">
+      <img src={HeroImg} alt="mountain peak" className="home-hero__img" />
+      <div className="home-hero__img__overlay"></div>
+      <div className="container home-hero__container">
+        <h1 className="home-hero__heading">
+          <span className="home-hero__heading__span">
+            <span className="home-hero__heading__span__inner">
+              Elevating Your
+            </span>
+          </span>
+          <span className="home-hero__heading__span">
+            <span className="home-hero__heading__span__inner">
+              Digital Presence
+            </span>
+          </span>
+        </h1>
+        <span className="home-hero__sub-heading">
+          Web Design, Development & Marketing Agency
+        </span>
+        <div className="home-hero__btn-wrapper">
+          <Button btnText="Get a Free Proposal" btnStyle="primary" rounded />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HomeHero;
