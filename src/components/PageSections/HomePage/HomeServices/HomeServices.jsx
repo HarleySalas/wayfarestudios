acaada6b90e57e13178c2728a8b68cb303a1a450
Harley Salas <harley@harleysalas.com>
2019-12-19 23:27:29 +0300

    initial HomeServices commit

---
+++ src/components/PageSections/HomePage/HomeServices/HomeServices.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeServices.scss";
+
+const HomeServices = () => {
+  return (
+    <div className="home-services">
+      <h1>I'm the services section</h1>
+      <p>
+        This is just a test paragraph to see what things look like when they're
+        all typed out on the page.
+      </p>
+    </div>
+  );
+};
+
+export default HomeServices;
