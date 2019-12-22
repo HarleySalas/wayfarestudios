3a13adc9673358532ccfd22c56692847a1936b1f
Harley Salas <harley@harleysalas.com>
2019-12-23 00:07:02 +0300

    added Footer (incomplete)

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container footer__container">
+        <h2 className="heading">Footer</h2>
+        <span>//Incomplete</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
