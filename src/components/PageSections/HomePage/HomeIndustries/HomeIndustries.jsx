2feaa529e00096c4afd71d8b1435c0d528718a02
Harley Salas <harley@harleysalas.com>
2019-12-23 00:09:23 +0300

    added HomeIndustries (Incomplete)

---
+++ src/components/PageSections/HomePage/HomeIndustries/HomeIndustries.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeIndustries.scss";
+
+const HomeIndustries = () => {
+  return (
+    <section className="home-industries">
+      <div className="container home-industries__container">
+        <h2>Industries We Serve</h2>
+        <span>//Incomplete</span>
+      </div>
+    </section>
+  );
+};
+
+export default HomeIndustries;
