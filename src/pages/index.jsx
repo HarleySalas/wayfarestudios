afae4a947407d82e61f50d218e491449e2f06e56
Harley Salas <harley@harleysalas.com>
2019-12-23 00:07:44 +0300

    added all components to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,22 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/Layout/Layout"
-import SEO from "../components/Layout/SEO/SEO"
+import Layout from "../components/Layout/Layout";
+import SEO from "../components/Layout/SEO/SEO";
 
+import HomeHero from "../components/PageSections/HomePage/HomeHero/HomeHero";
+import HomeMission from "../components/PageSections/HomePage/HomeMission/HomeMission";
+import HomeServices from "../components/PageSections/HomePage/HomeServices/HomeServices";
+import HomeIndustries from "../components/PageSections/HomePage/HomeIndustries/HomeIndustries";
+import HomeProposal from "../components/PageSections/HomePage/HomeProposal/HomeProposal";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Gatsby Boilerplate</h1>
+    <HomeHero />
+    <HomeMission />
+    <HomeServices />
+    <HomeIndustries />
+    <HomeProposal />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
