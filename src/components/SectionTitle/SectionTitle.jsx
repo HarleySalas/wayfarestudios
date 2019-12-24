8bd09fccc4c62450d6660b60aa9625b87ea0c0dd
Harley Salas <harley@harleysalas.com>
2019-12-24 06:43:00 +0300

    reusable title component

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import "./SectionTitle.scss";
+
+import { useObserver } from "../utils";
+
+const SectionTitle = ({ description, title, centered, margin }) => {
+  const ref = useRef(null);
+  const animated = useObserver(ref);
+  return (
+    <div
+      className={`section-title ${margin &&
+        "section-title--margin"} ${centered && "section-title--centered"}`}
+      ref={ref}
+    >
+      {description && (
+        <span className={`section-title__description ${animated && "active"}`}>
+          {description}
+        </span>
+      )}
+      <h2 className="section-title__title">
+        <span className="section-title__title__span">
+          <span
+            className={`section-title__title__span__inner ${animated &&
+              "active"}`}
+          >
+            {title}
+          </span>
+        </span>
+      </h2>
+      <span className={`section-title__line ${animated && "active"}`}></span>
+    </div>
+  );
+};
+
+export default SectionTitle;
