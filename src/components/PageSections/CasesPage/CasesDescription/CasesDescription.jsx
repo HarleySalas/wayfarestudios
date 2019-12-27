cec1dd1e2d44495df1ff031bc6c59d44487a19e7
Harley Salas <harley@harleysalas.com>
2019-12-27 16:52:37 +0300

    finished cases page

---
+++ src/components/PageSections/CasesPage/CasesDescription/CasesDescription.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PageDescription from "../../../PageDescription/PageDescription";
+
+const CasesDescription = () => {
+  return (
+    <PageDescription
+      description="Case Study"
+      heading="Coming Soon"
+      content="We're looking for businesses to work with, which will make for great success stories. Our first clients will serve as investments to us, by providing discounts of up to 10 times our normal rates. The future of our business depends on your success. By choosing to work with us early-on, you’ll receive our undivided attention. "
+    />
+  );
+};
+
+export default CasesDescription;
--- src/pages/cases.jsx
+++ src/pages/cases.jsx
@@ -5,12 +5,14 @@ import SEO from "../components/Layout/SEO/SEO";
 import ContactCTA from "../components/ContactCTA/ContactCTA";
 
 import CasesHeader from "../components/PageSections/CasesPage/CasesHeader/CasesHeader";
+import CasesDescription from "../components/PageSections/CasesPage/CasesDescription/CasesDescription";
 
 const cases = () => {
   return (
     <Layout>
       <SEO title="Cases" />
       <CasesHeader />
+      <CasesDescription />
       <ContactCTA />
     </Layout>
   );
