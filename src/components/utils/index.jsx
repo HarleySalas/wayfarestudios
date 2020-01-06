444eb9c4062baddaca4b990d290bd7fe1c6388ea
Harley Salas <harley@harleysalas.com>
2020-01-06 11:25:29 +0300

    added hook to determine when path changes

--- src/components/utils/index.jsx
+++ src/components/utils/index.jsx
@@ -1,4 +1,5 @@
 export { default as useObserver } from "./useObserver";
 export { default as useFormValidate } from "./useFormValidate";
 export { default as useToggle } from "./useToggle";
+export { default as useLocationPath } from "./useLocationPath";
 export { default as validateContact } from "./validateContact";
