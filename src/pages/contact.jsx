27e058765083746779429843a94d55507247d13d
Harley Salas <harley@harleysalas.com>
2020-01-28 22:42:11 +0300

    removed unnecessary elements from contactPage

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/Layout/SEO/SEO";
+import NavTopBg from "../components/NavTopBg/NavTopBg";
 
-import ContactHeader from "../components/PageSections/ContactPage/ContactHeader/ContactHeader";
+// import ContactHeader from "../components/PageSections/ContactPage/ContactHeader/ContactHeader";
 import ContactForm from "../components/PageSections/ContactPage/ContactForm/ContactForm";
 
 const contact = () => {
   return (
     <Layout>
       <SEO title="Contact" />
-      <ContactHeader />
+      <NavTopBg />
+      {/* <ContactHeader /> */}
 
       <ContactForm />
     </Layout>
