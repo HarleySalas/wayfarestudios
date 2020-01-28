c088e442723f3d780da088f340165ef999ec3e3f
Harley Salas <harley@harleysalas.com>
2020-01-28 22:43:10 +0300

    implemented gatsby-image

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import Img from "gatsby-image";
 import "./PageHeader.scss";
 
 const PageHeader = ({ img, alt, title, subtitle }) => {
   return (
     <section className="page-header">
       <div className="page-header__background__backdrop"></div>
-      <img className="page-header__background" src={img} alt={alt} />
+      {/* {img && <img className="page-header__background" src={img} alt={alt} />} */}
+      {img && (
+        <Img
+          fluid={img}
+          fadeIn={true}
+          durationFadeIn={500}
+          placeholderStyle={{ opacity: 0 }}
+          draggable={false}
+          alt={alt}
+          className="page-header__img-wrapper"
+          imgStyle={{
+            opacity: 0.72,
+            animation:
+              "fadeIn 2.24s cubic-bezier(0.165, 0.84, 0.44, 1) forwards",
+            userSelect: "none",
+          }}
+        />
+      )}
       <div className="page-header__container">
         <h1 className="page-header__title">
           <span className="page-header__title__span">
