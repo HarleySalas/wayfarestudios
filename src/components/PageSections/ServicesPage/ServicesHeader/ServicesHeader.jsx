cb699a3e84a0201706a5494dfb77111146f17875
Harley Salas <harley@harleysalas.com>
2020-01-28 22:43:40 +0300

    implemented gatsby-image and minor redesign

--- src/components/PageSections/ServicesPage/ServicesHeader/ServicesHeader.jsx
+++ src/components/PageSections/ServicesPage/ServicesHeader/ServicesHeader.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
-import HeroImg from "./assets/services-hero.jpg";
+// import HeroImg from "./assets/services-hero.jpg";
 
 import PageHeader from "../../../PageHeader/PageHeader";
 
 const ServicesHeader = () => {
+  const data = useStaticQuery(graphql`
+    query ServicesHeroImg {
+      image: file(relativePath: { eq: "services-hero.jpg" }) {
+        id
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
   return (
     <PageHeader
-      img={HeroImg}
+      img={data.image.childImageSharp.fluid}
       alt="New York City Skyline"
       title="Services"
-      subtitle="Create Your Future"
+      // subtitle="Create Your Future"
+      subtitle="From Vision To Reality"
     />
   );
 };
