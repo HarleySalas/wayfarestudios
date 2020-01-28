a84a45738f15b6e0de2ebe78837d9ed26db30385
Harley Salas <harley@harleysalas.com>
2020-01-28 22:40:25 +0300

    redesigned 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/Layout/SEO/SEO";
+import ContactCTA from "../components/ContactCTA/ContactCTA";
 
 import NotFound from "../components/PageSections/NotFound/NotFound";
 
@@ -9,6 +10,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <NotFound />
+    <ContactCTA />
   </Layout>
 );
 
