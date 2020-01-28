fac5972be3853a8dd37120c311351da4167e5ba0
Harley Salas <harley@harleysalas.com>
2020-01-28 22:42:18 +0300

    redesigned 404 page

--- src/components/PageSections/NotFound/NotFound.jsx
+++ src/components/PageSections/NotFound/NotFound.jsx
@@ -3,19 +3,12 @@ import "./NotFound.scss";
 
 import PageHeader from "../../PageHeader/PageHeader";
 
-import HeroImg from "./assets/404-img.jpg";
-
 import Button from "../../Button/Button";
 
 const NotFound = () => {
   return (
     <div>
-      <PageHeader
-        img={HeroImg}
-        alt="Lost In Antarctica"
-        title="Error Code: 404"
-        subtitle="Page Not Found"
-      />
+      <PageHeader title="Error Code: 404" subtitle="Page Not Found" />
       <div className="container not-found__container">
         <h3 className="not-found__title">Return to the Home Page</h3>
         <Button btnText="Home Page" linkTo="/" btnStyle="primary" />
