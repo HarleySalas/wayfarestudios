ae0b0b60a14fc0b9797c0d54550aa005d39967cb
Harley Salas <harley@harleysalas.com>
2020-01-28 22:43:54 +0300

    changed cases description

--- src/components/PageSections/CasesPage/CasesDescription/CasesDescription.jsx
+++ src/components/PageSections/CasesPage/CasesDescription/CasesDescription.jsx
@@ -7,7 +7,8 @@ const CasesDescription = () => {
     <PageDescription
       description="Case Study"
       heading="Coming Soon"
-      content="We're looking for businesses to work with, which will make for great success stories. Our first clients will serve as investments to us, by providing discounts of up to 10 times our normal rates. The future of our business depends on your success. By choosing to work with us early-on, you’ll receive our undivided attention. "
+      // content="We're looking for businesses to work with, which will make for great success stories. Our first clients will serve as investments to us, by providing discounts of up to 10 times our normal rates. The future of our business depends on your success. By choosing to work with us early-on, you’ll receive our undivided attention. "
+      content="We’re a new agency with a rock solid team. Though we don’t have the client history yet, we’ve got the experience to provide world-class results. By becoming one of our first clients, you’ll receive a 100% satisfaction guarantee, paying nothing unless you’re entirely satisfied with our work. We’ll stop at nothing, so that your success can be featured here, as a testament to our abilities."
     />
   );
 };
