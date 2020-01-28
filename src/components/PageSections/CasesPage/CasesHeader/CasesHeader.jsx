bbcb34b25e2f0449672d75c6408f61be0ed61624
Harley Salas <harley@harleysalas.com>
2020-01-28 22:44:07 +0300

    implemented gatsby-image and changed heading text

--- src/components/PageSections/CasesPage/CasesHeader/CasesHeader.jsx
+++ src/components/PageSections/CasesPage/CasesHeader/CasesHeader.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import PageHeader from "../../../PageHeader/PageHeader";
-import CasesImg from "./assets/cases-hero.jpg";
+// import CasesImg from "./assets/cases-hero.jpg";
 
 const CasesHeader = () => {
+  const data = useStaticQuery(graphql`
+    query CasesHeroImg {
+      image: file(relativePath: { eq: "cases-hero.jpg" }) {
+        id
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <PageHeader
-      img={CasesImg}
-      alt="Busy Businesses"
+      img={data.image.childImageSharp.fluid}
+      alt="Busy Business Street"
       title="Case Studies"
       subtitle="Partnerships That Thrive"
     />
