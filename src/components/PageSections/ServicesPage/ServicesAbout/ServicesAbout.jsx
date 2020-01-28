319a820c44aa7d42b1786bbadc1a924aff4321d3
Harley Salas <harley@harleysalas.com>
2020-01-28 22:44:47 +0300

    changed services content

--- src/components/PageSections/ServicesPage/ServicesAbout/ServicesAbout.jsx
+++ src/components/PageSections/ServicesPage/ServicesAbout/ServicesAbout.jsx
@@ -6,8 +6,10 @@ const ServicesAbout = () => {
   return (
     <PageDescription
       description="Services"
-      heading="Your Brand Is Everything"
-      content="It defines your business. Often, the first interaction someone has with you is online. Rather it be through social media, or your website, it will set the tone for how your audience perceives you. If you don’t stand out, potential clients will go to someone else who does. Our job is to help people discover you and make sure that you stand out when they do."
+      // heading="Your Brand Is Everything"
+      // content="It defines your business. Often, the first interaction someone has with you is online. Rather it be through social media, or your website, it will set the tone for how your audience perceives you. If you don’t stand out, potential clients will go to someone else who does. Our job is to help people discover you and make sure that you stand out when they do."
+      heading="Our Value Proposition"
+      content="Moving away from the industry-norm of simply marking projects as complete, Wayfare Studios focuses on longevity for the work we do, as well as our client relationships. Evaluating things from your perspective, planning for the future, and treating each client’s objectives as if they were our own is what we do. When working with us, you’ll always receive the greatest work we’re capable of providing."
     />
   );
 };
